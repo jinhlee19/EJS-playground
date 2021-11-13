4cb781035b86e7bc1772182575097287fe6d9024
rafy <jinhlee19@gmail.com>
2021-11-14 04:57:31 +0900

    savepoint 2.2

--- todolist/app.js
+++ todolist/app.js
@@ -1,48 +1,146 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
+const _ = require("lodash");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-app.get("/", function(req, res) {
+// ITEM SCHEMA
 
-const day = date.getDate();
+const itemsSchema = {
+	name: String,
+};
+const Item = mongoose.model("Item", itemsSchema);
 
-  res.render("list", {listTitle: day, newListItems: items});
+// LIST SCHEMA
 
+const listSchema = {
+	name: String,
+	items: [itemsSchema],
+};
+const List = mongoose.model("list", listSchema);
+
+// default items
+const item1 = new Item({ name: "Welcome to do your todo List!" });
+const item2 = new Item({ name: "hit the + button!" });
+const item3 = new Item({ name: "<-- hit" });
+
+const defaultItems = [item1, item2, item3];
+
+// mongoose find()
+app.get("/", function (req, res) {
+	//{} -> 모두 찾기, foundItems
+	Item.find({}, function (err, foundItems) {
+		if (foundItems.length === 0) {
+			Item.insertMany(defaultItems, function (err) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("Successfully saved default items to DB.");
+				}
+			});
+			res.redirect("/");
+		} else {
+			res.render("list", { listTitle: "Today", newListItems: foundItems });
+		}
+	});
 });
 
-app.post("/", function(req, res){
+//// Post + Save items (어디로?) 347
 
-  const item = req.body.newItem;
+app.post("/", function (req, res) {
+	const itemName = req.body.newItem;
+	const listName = req.body.list;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
+	const item = new Item({
+		// name: itemName
+		name: itemName,
+	});
+
+	if (listName === "Today") {
+		item.save();
+		res.redirect("/");
+	} else {
+		//Look for list matching listName
+		List.findOne({ name: listName }, function (err, foundList) {
+			//push the new item to the list that matches listName
+			foundList.items.push(item);
+			foundList.save();
+			//redirect to the custom page
+			res.redirect("/" + listName);
+		});
+	}
+});
+
+//// Delete - 345, 348
+app.post("/delete", function (req, res) {
+	const checkedItemId = req.body.checkbox;
+	const listName = req.body.listName;
+
+	if (listName === "Today"){
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+			if (!err) {
+				console.log("task has been done.");
+				res.redirect("/");
+			}
+		});
+  }else {
+    List.findOneAndUpdate(
+      {name:listName},
+      // 3 layered query 케이크 -> {}   // list schema의 items, 바로위 const checkItemId의 id으로 걸러서 $pull operation 실행 
+      {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+        if (!err){
+          res.redirect("/" + listName);
+        }
+      });
   }
+		
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+//// Route Parameter 로 custom List 만들기 - 346
+
+app.get("/:customListName", function (req, res) {
+	const customList = _.capitalize(req.params.customListName);
+
+	List.findOne({ name: customList }, function (err, foundList) {
+		if (!err) {
+			if (!foundList) {
+				//if foundlist doesn't exist!! !로 존재 유무를 확인
+				//Create a new List
+				const list = new List({ 
+					name: customList, // 구분을 위해 강의와 다르게 표시
+					items: defaultItems,
+				});
+				list.save();
+				res.redirect("/" + customList);
+			} else {
+				//Show an existing list (이미 존재하므로 -> 추가 생성 방지.)
+				res.render("list", {
+					listTitle: foundList.name,
+					newListItems: foundList.items,
+				});
+			}
+		}
+	});
 });
 
-app.get("/about", function(req, res){
-  res.render("about");
+app.get("/work", function (req, res) {
+	res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.get("/about", function (req, res) {
+	res.render("about");
+});
+
+app.listen(3000, function () {
+	console.log("Server started on port 3000");
 });
